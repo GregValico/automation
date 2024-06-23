1aaf7fa850aadd20639942435d62d8726846e09c
Greg Valico <greg.valico@gmail.com>
2024-06-23 00:41:49 -0500

    Changed to single quotations

--- scripts/loadbalancer.sh
+++ scripts/loadbalancer.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-gcloud compute ssh --zone "$REGION-a" "gke-jump-$CLUSTER_NAME" --project "$PROJECT_NAME" --command "
+gcloud compute ssh --zone "$REGION-a" "gke-jump-$CLUSTER_NAME" --project "$PROJECT_NAME" --command '
   
   sleep 20
 
   echo From inside the VM, the PROXY IP is $PROXY_IP
   echo Cluster name is $CLUSTER_NAME
   echo Project name is $PROJECT_NAME
-"
+'
