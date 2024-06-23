7b7579a09bdf372d132394bb1a06d2828a27630f
Greg Valico <greg.valico@gmail.com>
2024-06-23 00:59:37 -0500

    Latest changes

--- scripts/loadbalancer.sh
+++ scripts/loadbalancer.sh
@@ -5,10 +5,18 @@ gcloud compute ssh --zone "$REGION-a" "gke-jump-$CLUSTER_NAME" --project "$PROJE
   sleep 20
 
   hostname
+  gcloud container clusters get-credentials $CLUSTER_NAME --region $REGION --project $PROJECT_NAME
 
   echo From inside the VM, the PROXY IP is $PROXY_IP
   echo Cluster name is $CLUSTER_NAME
   echo Project name is $PROJECT_NAME
+
+  cat <<EOF | kubectl apply -f -
+  apiVersion: v1
+  kind: Namespace
+  metadata:
+    name: $PROJECT_NAME
+  EOF
 
 
 "
