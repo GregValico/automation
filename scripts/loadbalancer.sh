fe0daf89d59771fbcb5da4b3fa2e3a48793909cd
Greg Valico <greg.valico@gmail.com>
2024-06-23 01:05:34 -0500

    Latest changes

--- scripts/loadbalancer.sh
+++ scripts/loadbalancer.sh
@@ -4,19 +4,55 @@ gcloud compute ssh --zone "$REGION-a" "gke-jump-$CLUSTER_NAME" --project "$PROJE
   
   sleep 20
 
-  hostname
   gcloud container clusters get-credentials $CLUSTER_NAME --region $REGION --project $PROJECT_NAME
-
-  echo From inside the VM, the PROXY IP is $PROXY_IP
-  echo Cluster name is $CLUSTER_NAME
-  echo Project name is $PROJECT_NAME
 
   cat <<EOF | kubectl apply -f -
   apiVersion: v1
   kind: Namespace
   metadata:
-    name: $PROJECT_NAME
+    name: test
 EOF
-
-
+  cat <<EOF | kubectl apply -f -
+  apiVersion: apps/v1
+  kind: Deployment
+  metadata:
+    name: nginx-deployment
+    namespace: test
+    labels:
+      app: nginx
+  spec:
+    replicas: 3
+    selector:
+      matchLabels:
+        app: nginx
+    template:
+      metadata:
+        labels:
+          app: nginx
+      spec:
+        containers:
+        - name: nginx
+          image: nginx:latest
+          ports:
+          - containerPort: 80
+EOF
+  cat <<EOF | kubectl apply -f -
+  apiVersion: v1
+  kind: Service
+  metadata:
+    name: nginx-service
+    namespace: test
+    labels:
+      app: nginx
+    annotations:
+      cloud.google.com/load-balancer-type: "Internal"
+  spec:
+    type: LoadBalancer
+    loadBalancerIP: $PROXY_IP
+    ports:
+    - port: 80
+      targetPort: 80
+    selector:
+      app: nginx
+EOF
 "
