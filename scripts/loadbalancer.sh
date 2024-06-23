2b18e8800af323e59cb5a18d82d179ca46d82b06
Greg Valico <greg.valico@gmail.com>
2024-06-23 00:09:28 -0500

    Added missing quotations

--- scripts/loadbalancer.sh
+++ scripts/loadbalancer.sh
@@ -4,62 +4,62 @@ gcloud compute ssh --zone "$REGION-a" "gke-jump-$CLUSTER_NAME" --project "$PROJE
   
   sleep 20
 
-  export PROXY_IP="${PROXY_IP}
-  export CLUSTER_NAME="${CLUSTER_NAME}
-  export PROJECT_NAME="${PROJECT_NAME}
+  export PROXY_IP="${PROXY_IP}"
+  export CLUSTER_NAME="${CLUSTER_NAME}"
+  export PROJECT_NAME="${PROJECT_NAME}"
 
   echo The api proxy IP is $PROXY_IP
 
   gcloud config set project $PROJECT_NAME
   gcloud container clusters get-credentials $CLUSTER_NAME --region=$REGION --project=$PROJECT_NAME
   
-  #   cat <<EOF | kubectl apply -f -
-  #   apiVersion: v1
-  #   kind: Namespace
-  #   metadata:
-  #     name: test
-  # EOF
-  #   cat <<EOF | kubectl apply -f -
-  #   apiVersion: apps/v1
-  #   kind: Deployment
-  #   metadata:
-  #     name: nginx-deployment
-  #     namespace: test
-  #     labels:
-  #       app: nginx
-  #   spec:
-  #     replicas: 3
-  #     selector:
-  #       matchLabels:
-  #         app: nginx
-  #     template:
-  #       metadata:
-  #         labels:
-  #           app: nginx
-  #       spec:
-  #         containers:
-  #         - name: nginx
-  #           image: nginx:latest
-  #           ports:
-  #           - containerPort: 80
-  # EOF
-  #   cat <<EOF | kubectl apply -f -
-  #   apiVersion: v1
-  #   kind: Service
-  #   metadata:
-  #     name: nginx-service
-  #     namespace: test
-  #     labels:
-  #       app: nginx
-  #     annotations:
-  #       cloud.google.com/load-balancer-type: "Internal"
-  #   spec:
-  #     type: LoadBalancer
-  #     loadBalancerIP: $PROXY_IP
-  #     ports:
-  #     - port: 80
-  #       targetPort: 80
-  #     selector:
-  #       app: nginx
-  # EOF
+  cat <<EOF | kubectl apply -f -
+  apiVersion: v1
+  kind: Namespace
+  metadata:
+    name: test
+EOF
+  cat <<EOF | kubectl apply -f -
+  apiVersion: apps/v1
+  kind: Deployment
+  metadata:
+    name: nginx-deployment
+    namespace: test
+    labels:
+      app: nginx
+  spec:
+    replicas: 3
+    selector:
+      matchLabels:
+        app: nginx
+    template:
+      metadata:
+        labels:
+          app: nginx
+      spec:
+        containers:
+        - name: nginx
+          image: nginx:latest
+          ports:
+          - containerPort: 80
+EOF
+  cat <<EOF | kubectl apply -f -
+  apiVersion: v1
+  kind: Service
+  metadata:
+    name: nginx-service
+    namespace: test
+    labels:
+      app: nginx
+    annotations:
+      cloud.google.com/load-balancer-type: "Internal"
+  spec:
+    type: LoadBalancer
+    loadBalancerIP: $PROXY_IP
+    ports:
+    - port: 80
+      targetPort: 80
+    selector:
+      app: nginx
+EOF
 '
