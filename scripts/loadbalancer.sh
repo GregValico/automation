75ccc43c1d4c5d07b93d052a07fe71e2dc458085
Greg Valico <greg.valico@gmail.com>
2024-06-23 00:26:34 -0500

    Added missing quotations

--- scripts/loadbalancer.sh
+++ scripts/loadbalancer.sh
@@ -5,10 +5,6 @@ gcloud compute ssh --zone "$REGION-a" "gke-jump-$CLUSTER_NAME" --project "$PROJE
   sleep 20
 
   export PROXY_IP='${{ env.PROXY_IP }}'
-  export CLUSTER_NAME='${{ env.CLUSTER_NAME }}'
-  export PROJECT_NAME='${{ env.PROJECT_NAME }}'
 
-  echo \$PROXY_IP
-
-  gcloud config set project \$PROJECT_NAME
+  echo $PROXY_IP
 "
